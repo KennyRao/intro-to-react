0cf140fe0bdeba9e24103c3863e3255d0518bf42
Weikai Rao <weikairao@gmail.com>
2024-04-04 09:17:12 -0500

    Add TodoItem component

---
+++ src/TodoItem.jsx
@@ -0,0 +1,11 @@
+import "./TodoItem.css"
+
+function TodoItem(){
+    return (
+        <article className="todo-item">
+            Hello World!
+        </article>
+    );
+}
+
+export default TodoItem;
